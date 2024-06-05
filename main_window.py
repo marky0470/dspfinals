7a4ae62822e5a6823d5130a74892652e03315cca
Xyrone Sanchez <sanchezxyronejei@gmail.com>
2024-06-05 20:28:46 +0800

    update my lodicakes

--- main_window.py
+++ main_window.py
@@ -27,7 +27,7 @@ class MainWindow(tk.Frame):
 
         button1 = ttk.Button(buttons_frame, width=40, text="Analog to Digital Converter", command=lambda: controller.show_frame(ADC))
         button2 = ttk.Button(buttons_frame, width=40, text="Finite Impulse Response", command=lambda: controller.show_frame(fir))
-        button3 = ttk.Button(buttons_frame, width=40, text="Moving Average", command=lambda: controller.show_frame(mov))
+        button3 = ttk.Button(buttons_frame, width=40, text="Moving Average", command=lambda: controller.show_frame(MOV))
         button4 = ttk.Button(buttons_frame, width=40, text="Low-Pass <-> High-Pass", command=lambda: controller.show_frame(lhc))
 
         button1.grid(row=0, column=0, pady=10, padx=10)
@@ -39,5 +39,5 @@ class MainWindow(tk.Frame):
 
 from adc import ADC
 from fir import fir
-from mov import mov
+from mov import MOV
 from lhc import lhc
